65a40692719e22e96d42bc486aa77bfa17804d19
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-23 14:15:33 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
 
   return (
     <div className=''>
-        ehfghfghjhg
+         <Button>Click me</Button>
     </div>
   )
 }
