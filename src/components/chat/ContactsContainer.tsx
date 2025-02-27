d0d63eeb14b005195f69017aa1ff4d6ef971f1dc
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-27 14:43:29 +0200

    done

--- src/components/chat/ContactsContainer.tsx
+++ src/components/chat/ContactsContainer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import obscura from "../../../public/obscura.svg";
+import Title from "./Title";
 
 const ContactsContainer = () => {
   return (
     <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full'>
-        ContactsContainer
+        {/* LOGO start */}
+        <div className='flex items-center pt-3 pl-5'>
+            <img src={obscura} alt="Logo" className="w-8 h-8 rounded-full bg-white"/>
+            <div className="text-black font-bold text-4xl text-white">
+                bscura
+            </div>
+        </div>
+        {/* LOGO END */}
+        <div className="my-5">
+            <div className="flex items-center justify-between px-10">
+                <Title text="Direct Messages"/>
+            </div>
+        </div>
+        <div className="my-5">
+            <div className="flex items-center justify-between px-10">
+                <Title text="Channels"/>
+            </div>
+        </div>
     </div>
   )
 }
